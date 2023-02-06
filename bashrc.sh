f1ff30d0a4d3862e3f478b1b7ee7c5681ad619a0
Jung Sangwoo <holyavarice@gmail.com>
2023-02-06 13:07:29 +0900

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# >>> alias for micromamba >>>
+if [ -n "${MAMBA_EXE}" ]
+then
+    alias mm='micromamba'
+    complete -o default -F _umamba_bash_completions mm
+fi
+# <<< alias for micromamba <<<
+
 # Append git status to PS1
-PS1=$(echo $PS1 | sed -e 's/\\\$/$(__git_ps1)\\$ /')
+if which git > /dev/null 2>&1
+then
+    PS1=$(echo $PS1 | sed -e 's/\\\$/$(__git_ps1)\\$ /')
+fi
