18a116031240991727863d0c2d8dcf0c057b43c4
Jung Sangwoo <holyavarice@gmail.com>
2024-04-02 16:02:09 +0900

    Update link_apt-fast.sh fix typo

--- link_apt-fast.sh
+++ link_apt-fast.sh
@@ -58,7 +58,7 @@ if ! ${SUDO}which apt-fast; then
     else
         ${SUDO}tee -a "_SPLITCON=${NUM}" /etc/apt-fast.conf;
     fi
-    if [ $NUM -bt 16 ]; then
+    if [ $NUM -gt 16 ]; then
         NUM=16
     fi
     if ${SUDO}grep "^_MAXCONPERSRV=" /etc/apt-fast.conf; then
