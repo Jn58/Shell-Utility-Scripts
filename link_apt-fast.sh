b43d27fb1094a3f7e35daf3b030f691ad48ad6d0
Jung Sangwoo <holyavarice@gmail.com>
2024-04-02 15:51:37 +0900

    Update link_apt-fast.sh make it run without saving

--- link_apt-fast.sh
+++ link_apt-fast.sh
@@ -1,11 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-APT_GET=$(which apt-get)
+if [ $(id -u) -ne 0 ]; then
+    SUDO="sudo "
+else
+    SUDO=""
+fi
+
+APT_GET=$(${SUDO}which apt-get)
 if [ -L $APT_GET ]; then
-    LINK=$(readlink $APT_GET)
-    if [ $(basename $LINK) == "apt-fast" ]; then
+    LINK=$(${SUDO}readlink $APT_GET)
+    if [ $(${SUDO}basename $LINK) = "apt-fast" ]; then
         echo 'apt-fast is already applied'
         exit 0
     else
@@ -14,52 +20,49 @@ if [ -L $APT_GET ]; then
     fi
 fi
 
-if [ $(id -u) -ne 0 ]; then
-    exec sudo /bin/sh "$0" "$@"
-fi
 
-APT_PATH=$(dirname $APT_GET)
-FAST_PATH=$(echo $PATH | cut -d: -f 1)
+APT_PATH=$(${SUDO}dirname $APT_GET)
+FAST_PATH=$(${SUDO}bash -c "echo \$PATH | cut -d: -f 1")
 
-if [ $APT_PATH == $FAST_PATH ]; then
+if [ $APT_PATH = $FAST_PATH ]; then
     echo "apt-get is in first \$PATH";
     exit 1;
 fi
 
-if ! which apt-fast; then
-    if ! which add-apt-repository; then
-        apt-get update;
-        apt-get install -y software-properties-common;
+if ! ${SUDO}which apt-fast; then
+    if ! ${SUDO}which add-apt-repository; then
+        ${SUDO}apt-get update;
+        ${SUDO}apt-get install -y software-properties-common;
     fi
-    add-apt-repository -y ppa:apt-fast/stable
-    apt-get update;
-    DEBIAN_FRONTEND=noninteractive apt-get install -y apt-fast;
-    if grep "^_APTMGR=" /etc/apt-fast.conf; then
-        sed -i "s|^_APTMGR=.*|_APTMGR=${APT_GET}|" /etc/apt-fast.conf;
-    else 
-        echo "_APTMGR=${APT_GET}" >> /etc/apt-fast.conf;
-    fi
-    if grep "^DOWNLOADBEFORE=" /etc/apt-fast.conf; then
-        sed -i "s|^DOWNLOADBEFORE=.*|DOWNLOADBEFORE=true|" /etc/apt-fast.conf;
+    ${SUDO}add-apt-repository -y ppa:apt-fast/stable
+    ${SUDO}apt-get update;
+    ${SUDO}bash -c "DEBIAN_FRONTEND=noninteractive apt-get install -y apt-fast";
+    if ${SUDO}grep "^_APTMGR=" /etc/apt-fast.conf; then
+        ${SUDO}sed -i "s|^_APTMGR=.*|_APTMGR=${APT_GET}|" /etc/apt-fast.conf;
     else
-        echo "DOWNLOADBEFORE=true" >> /etc/apt-fast.conf;
+        ${SUDO}tee -a "_APTMGR=${APT_GET}" /etc/apt-fast.conf;
+    fi
+    if ${SUDO}grep "^DOWNLOADBEFORE=" /etc/apt-fast.conf; then
+        ${SUDO}sed -i "s|^DOWNLOADBEFORE=.*|DOWNLOADBEFORE=true|" /etc/apt-fast.conf;
+    else
+        ${SUDO}tee -a "DOWNLOADBEFORE=true" /etc/apt-fast.conf;
     fi
     NUM=$(nproc)
-    if grep "^_MAXNUM=" /etc/apt-fast.conf; then
-        sed -i "s|^_MAXNUM=.*|_MAXNUM=${NUM}|" /etc/apt-fast.conf;
-    else 
-        echo "_MAXNUM=${NUM}" >> /etc/apt-fast.conf;
+    if ${SUDO}grep "^_MAXNUM=" /etc/apt-fast.conf; then
+        ${SUDO}sed -i "s|^_MAXNUM=.*|_MAXNUM=${NUM}|" /etc/apt-fast.conf;
+    else
+        ${SUDO}tee -a "_MAXNUM=${NUM}" /etc/apt-fast.conf;
     fi
-    if grep "^_SPLITCON=" /etc/apt-fast.conf; then
-        sed -i "s|^_SPLITCON=.*|_SPLITCON=${NUM}|" /etc/apt-fast.conf;
-    else 
-        echo "_SPLITCON=${NUM}" >> /etc/apt-fast.conf;
+    if ${SUDO}grep "^_SPLITCON=" /etc/apt-fast.conf; then
+        ${SUDO}sed -i "s|^_SPLITCON=.*|_SPLITCON=${NUM}|" /etc/apt-fast.conf;
+    else
+        ${SUDO}tee -a "_SPLITCON=${NUM}" /etc/apt-fast.conf;
     fi
-    if grep "^_MAXCONPERSRV=" /etc/apt-fast.conf; then
-        sed -i "s|^_MAXCONPERSRV=.*|_MAXCONPERSRV=${NUM}|" /etc/apt-fast.conf;
-    else 
-        echo "_MAXCONPERSRV=${NUM}" >> /etc/apt-fast.conf;
+    if ${SUDO}grep "^_MAXCONPERSRV=" /etc/apt-fast.conf; then
+        ${SUDO}sed -i "s|^_MAXCONPERSRV=.*|_MAXCONPERSRV=${NUM}|" /etc/apt-fast.conf;
+    else
+        ${SUDO}tee -a "_MAXCONPERSRV=${NUM}" /etc/apt-fast.conf;
     fi
 fi
 
-ln -s $(which apt-fast) $FAST_PATH/apt-get
+${SUDO}ln -s $(${SUDO}which apt-fast) $FAST_PATH/apt-get
